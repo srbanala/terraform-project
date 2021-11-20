0fb82f8785e25287d7c86d1fbbe45f4837071f02
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-20 11:07:10 -0500

    amin

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+   organization = "Docker-nginx"
+    workspaces {
+      name = "terraform-project"
+    }
+  }
