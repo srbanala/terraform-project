f7c80f5da5ba3a94cbd353b2a46e2421738248f1
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-20 12:23:07 -0500

    new

--- main.tf
+++ main.tf
@@ -16,18 +16,6 @@ terraform {
   required_version = ">=0.14.9"
 }
 
-variable  "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
-
-provider "aws" {
-  profile = "default"
-  region  = var.aws_region
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   acl = "public-read"
     versioning {
