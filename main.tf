b44b36753a367018cf918c943494f72d673ba1bc
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-20 11:57:31 -0500

    new file

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
+
 terraform {
-  required_providers {
-     aws ={
-       source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-}
   backend "remote" {
-   organization = "Docker-nginx"
+    organization = "docker-nginx"
     workspaces {
       name = "terraform-project"
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+    }
+  }
+  required_version = ">=0.14.9"
+}
